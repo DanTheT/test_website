ac2bc015244e437d47bcf3b69d7322a0591164ad
danielwongtingpoh <88531063+DanTheT@users.noreply.github.com>
2022-04-09 17:54:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cf403jikrrll.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "danielwongtingpoh-employee"
+customregion = "us-east-1"
